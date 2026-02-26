43950573e91b9e145eaa78e1bff5e349b1789292
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-02-26 15:36:18 +0530

    Add authentication routes for OTP handling and user verification; integrate routes into main application

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-import express from "express"
+import express from "express";
+import routes from "./routes/index.js";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
+
+app.use("/api", routes);
 
 export default app;
