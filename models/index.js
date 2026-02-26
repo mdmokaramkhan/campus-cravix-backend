56e530e3d42707c21d87b6f381fbb784d4cb9bc3
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-02-26 14:33:36 +0530

    Add Order and Payment models with schemas; update index.js to export new models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
 export { default as User } from "./User.js";
 export { default as Vendor } from "./Vendor.js";
 export { default as MenuItem } from "./MenuItem.js";
+export { default as Order } from "./Order.js";
+export { default as Payment } from "./Payment.js";
