887f56ee6d9818b0554ebdfcb54c726ff0d7700d
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-02-26 13:45:59 +0530

    feat: add Cloudinary dependency and update logging in configuration files

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -7,9 +7,9 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-console.log('Cloudinary configured');
-console.log('CLOUDINARY_CLOUD_NAME: ' + process.env.CLOUDINARY_CLOUD_NAME);
-console.log('CLOUDINARY_API_KEY: ' + process.env.CLOUDINARY_API_KEY);
-console.log('CLOUDINARY_API_SECRET: ' + process.env.CLOUDINARY_API_SECRET);
+// console.log('Cloudinary configured');
+// console.log('CLOUDINARY_CLOUD_NAME: ' + process.env.CLOUDINARY_CLOUD_NAME);
+// console.log('CLOUDINARY_API_KEY: ' + process.env.CLOUDINARY_API_KEY);
+// console.log('CLOUDINARY_API_SECRET: ' + process.env.CLOUDINARY_API_SECRET);
 
 export default cloudinary;
--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
-        console.log(` ✅ MongoDB Connected Successfully`);
+        console.log(`✅ MongoDB Connected Successfully`);
     } catch (error) {
         console.error(`❌ MongoDB connection error: ${error.message}`);
         process.exit(1);
