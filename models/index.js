1ff1b0afa9a9c4ba02501a74ef4b3472708054cc
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-02-26 14:36:14 +0530

    Add Coupon, OfferBanner, and Review models with schemas; update index.js to export new models

--- models/index.js
+++ models/index.js
@@ -3,3 +3,6 @@ export { default as Vendor } from "./Vendor.js";
 export { default as MenuItem } from "./MenuItem.js";
 export { default as Order } from "./Order.js";
 export { default as Payment } from "./Payment.js";
+export { default as Coupon } from "./Coupon.js";
+export { default as OfferBanner } from "./OfferBanner.js";
+export { default as Review } from "./Review.js";
