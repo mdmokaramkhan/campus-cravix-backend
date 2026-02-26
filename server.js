1071ab2d5de85be52a8f5cef1cd94496a3b126d8
mdmokaramkhan <mohammadmokaram23@lpu.in>
2026-02-26 13:39:44 +0530

    feat: database and cloudinary setup completed

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
+
+// Connect MongoDB
+connectDB()
 
 app.listen(PORT, () => {
     console.log(`✅ Server started and running on PORT : ${PORT}`)
